af2f68b08b3cd23b3f51858cb218013522e2d109
januarelsan <januarelsan@gmail.com>
2021-01-21 00:51:13 +0700

    Add Email Validator

--- app/Http/Controllers/CreatorController.php
+++ app/Http/Controllers/CreatorController.php
@@ -3,20 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 use App\User;
 
 class CreatorController extends Controller
 {
     //
-    public function join()
+    public function join(Request $request)
     {
+     
+        
+        
+        //check unique
+        $validated = $request->validate([
+            'email' => 'required|unique:users|max:255',            
+        ]);
+
         $data=[
-            'email'=> request("email")            
+            'email'=> $request->email            
         ];
 
         User::create($data);
-        
-        return redirect('home')->with('popup', 'open');
+
+        // try {
+        //     $data=[
+        //         'email'=> $request->email            
+        //     ];
+    
+        //     User::create($data);
+        // } catch (Modelnot $exception) {
+
+        //     return back()->withError($exception->getMessage())->withInput();
+
+        // }
         
         
     }

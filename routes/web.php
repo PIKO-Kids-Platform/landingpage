c786d5a575c0b9cd11b13feac412e93eb7773dd8
januarelsan <januarelsan@gmail.com>
2021-01-20 19:03:28 +0700

    Add Routes
    - /home
    - /join

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::post('/join', 'CreatorController@join')->name('join');

7ed2ab1e444e6f1ec67bd7a0ea1e36b856984982
januarelsan <januarelsan@gmail.com>
2021-02-21 19:00:29 +0700

    Add Term and Condition View

---
+++ resources/views/tos.blade.php
@@ -0,0 +1,267 @@
+
+@extends('layouts.main')
+
+@section('content')
+<!-- ***** Breadcrumb Area Start ***** -->
+<section class="section breadcrumb-area bg-overlay d-flex align-items-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <!-- Breamcrumb Content -->
+                <div class="breadcrumb-content d-flex flex-column align-items-center text-center">
+                    <h3 class="text-white">Syarat dan Ketentuan</h3>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ***** Breadcrumb Area End ***** -->
+
+<!-- ***** Blog Area Start ***** -->
+<section id="blog" class="section blog-area ptb_100">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-12">
+                <!-- Single Blog Details -->
+                <article class="single-blog-details ">
+                    
+                    <!-- Blog Content -->
+                    <div class="blog-content sApp-blog">
+                        <!-- Meta Info -->
+                        <div class="meta-info d-flex flex-wrap align-items-center py-2">
+                            <ul>
+                                <li class="d-inline-block p-2">Version <a href="#">1.0</a></li>
+                                <li class="d-inline-block p-2"><a href="#">14 Feb, 2021</a></li>                                
+                            </ul>
+                            
+                        </div>
+                        <!-- Blog Details -->
+                        <div class="blog-details">
+                            <h3 class="blog-title py-2 py-sm-3"><a href="#">PIKO Early Adaptors Program - Syarat dan Ketentuan</a></h3>
+                            
+                            <blockquote class="blockquote px-4 py-3 my-3 ml-4">
+                                <p class="d-none d-sm-block">
+                                    Syarat dan ketentuan ini adalah kesepakatan antara kreator dan PIKO Kids Platform (disebut sebagai “PIKO” atau “Kami”). 
+                                    <br>
+                                    PIKO Early Adopters Program (disebut sebagai Program) dijalankan oleh PIKO.
+                                </p>
+                            </blockquote>                            
+                            
+                            <h3>1. Definisi</h3>
+
+                            <div class="px-4 my-3 ml-4">
+                                <b>a. PIKO Kids Platform</b>
+                                <p>
+                                    PIKO Kids Platform adalah mobile application dan/atau website application dengan alamat <a href="{{route('index')}}">www.haipiko.com</a>
+                                </p>                                
+                            </div>                            
+                            <div class="px-4 my-3 ml-4">
+                                <b>b. Kreator</b>
+                                <p>
+                                    Kreator adalah badan hukum atau individu yang telah mendaftar pada PIKO.
+                                </p>
+                            </div>
+                            <div class="px-4 my-3 ml-4">
+                                <b>c. Program</b>
+                                <p>
+                                    “PIKO Early Adopters Program” adalah salah satu bagian dari Market Research PIKO yang bertujuan untuk mengumpulkan, menganalisis                                     
+                                    dan menafsirkan informasi suatu pasar, produk atau layanan yang akan ditawarkan untuk dijual di pasar tersebut. Penelitian ini mencakup karakteristik, spending habits, lokasi dan kebutuhan dari target pasar PIKO dan industri secara keseluruhan. Program ini tidak melakukan aktifitas monetisasi.
+                                </p>
+                            </div>
+                            <div class="px-4 my-3 ml-4">
+                                <b>d. Buku</b>
+                                <p>
+                                    Buku digital yang di diserahkan pada PIKO (disebut sebagai “Buku kreator” atau “Buku yang diserahkan”).
+                                </p>
+                            </div>
+                            {{-- End of Part 1 --}}
+
+                            <h3>2. Kekayaan Intelektual </h3>
+
+                            <div class="px-4 my-3 ml-4">                                
+                                <p>
+                                    <b>a.</b> Kreator menjamin sebagai pemilik sah dari Buku yang diserahkan pada PIKO.
+                                </p>                                
+                            </div>                            
+                            <div class="px-4 my-3 ml-4">                                
+                                <p>
+                                    <b>b.</b> Kreator menjamin Buku yang diserahkan tidak memiliki ikatan atau perjanjian dengan pihak lain yang melarang Buku tersebut untuk diserahkan pada PIKO.
+                                </p>                                
+                            </div>                            
+                            <div class="px-4 my-3 ml-4">                                
+                                <p>
+                                    <b>c.</b> Kreator memberikan jaminan bahwa karangannya tidak bertentangan dengan norma-norma, tidak menyinggung hak cipta orang lain dan tidak memuat hal-hal yang merugikan nama baik pihak lain.
+                                </p>                                
+                            </div>            
+                            <div class="px-4 my-3 ml-4">                                
+                                <p>
+                                    <b>d.</b> Kreator bertanggung jawab sepenuhnya apabila di kemudian hari terjadi gugatan oleh pihak lain atas isi Buku tersebut.                
+                                </p>                                
+                            </div>   
+                            <div class="px-4 my-3 ml-4">                                
+                                <p>
+                                    <b>e.</b> Buku yang diserahkan pada PIKO tetap sepenuhnya menjadi kekayaan intelektual dari kreator. Kreator tetap dengan bebas melakukan apapun terhadap Buku kreator secara sepihak dan tanpa ijin dari PIKO.
+                                </p>                                
+                            </div>     
+                            {{-- End of Part 2 --}}  
+                            
+                            <h3>3. Penggunaan Buku </h3>
+
+                            <div class="px-4 my-3 ml-4">   
+                                                             
+                                <p>
+                                    <b>a.</b> PIKO dapat menggunakan Buku untuk menjadi bahan menjalankan PIKO Early Adopters Program.
+                                </p>                                
+                            </div>                                                          
+                            {{-- End of Part 3 --}}   
+                            
+                            <h3>4. Data Penelitian dan Info Program </h3>
+                            <div class="my-3 ml-4">   
+                                <p>
+                                    PIKO memiliki prinsip keterbukaan dalam menjalan program ini. Maka dari itu, keterbukaan data dan informasi dari program ini menjadi salah satu perhatian Kami.
+                                </p>                                
+                                <div class="px-4 my-3 ml-4">                                                                
+                                    <p>
+                                        <b>a.</b> Kreator akan mendapatkan data hasil penelitian program ini. Data yang akan didapatkan sebagai berikut:
+                                    </p>     
+                                    <div class="px-4 my-3 ml-4">                                                                
+                                        <p>
+                                            <b>i.</b> Jumlah Pengguna PIKO.
+                                        </p>                                                                      
+                                    </div>
+                                    <div class="px-4 my-3 ml-4">                                                                
+                                        <p>
+                                            <b>ii.</b> Jumlah "Love" pada Buku.
+                                        </p>                                                                      
+                                    </div>  
+                                    <div class="px-4 my-3 ml-4">                                                                
+                                        <p>
+                                            <b>iii.</b> Jumlah pembaca pada Buku.
+                                        </p>                                                                      
+                                    </div>  
+                                    <div class="px-4 my-3 ml-4">                                                                
+                                        <p>
+                                            <b>iv.</b> Ulasan Buku.
+                                        </p>                                                                      
+                                    </div>    
+                                </div>   
+                                <div class="px-4 my-3 ml-4">                                                                                                    
+                                    <p>
+                                        <b>b.</b> Kreator juga akan mendapatkan roadmap, berita, informasi, dan progress dari berjalannya program ini.
+                                    </p> 
+                                </div>                             
+                                
+                            </div>                                                          
+                            {{-- End of Part 4 --}}  
+
+                            <h3>5. <i>Reward</i> & <i>Fee</i> </h3>
+                            <div class="my-3 ml-4">   
+                                <p>
+                                    Sebagaimana yang disebutkan pada Bagian ke-1 (Definisi) butir c. (Program), PIKO menjalankan PIKO Early Adopters Program untuk melakukan penelitian dan tidak menjalankan aktifitas monetisasi, maka dari itu program ini tidak memiliki <i>fee</i> untuk buku yang diserahkan pada PIKO.                                                                                                                                                                                     
+                                </p>
+                                <br>     
+                                <p>
+                                    Salah satu target penelitian ini adalah menentukan skema <i>fee</i> yang proporsional yang akan diberikan pada buku yang diserahkan pada PIKO saat program ini berakhir. Namun Buku yang diserahkan pada periode program ini akan mendapatkan rewards sebagai berikut:                                
+                                </p>                                
+                                
+                                <div class="px-4 my-3 ml-4">                                                                
+                                    <b>a. <i>Reward</i></b>
+                                    <p>
+                                        Mendapatkan <i>fee</i> 150% dari skema <i>fee</i> yang akan diterapkan.                                                                                
+                                    </p>
+                                    <br>
+                                    <p>
+                                        Contoh Skema:
+                                        <br>
+                                        Jika PIKO membayar <i>fee</i> Rp1000 per-buku yang dibaca oleh pengguna, maka buku yang diserahkan pada program ini akan mendapatkan Rp1500 per-buku yang dibaca.
+                                    </p>
+                                    <br>
+                                    <p>                                        
+                                        Contoh Kasus:
+                                        <br>
+                                        Jika Buku berjudul “Belajar Menghitung” diserahkan pada periode program ini berjalan. Buku tersebut telah dibaca 3000 kali dalam sebulan, maka total <i>fee</i> yang didapatkan buku tersebut sebesar 3000 x Rp1500 yaitu Rp4.500.000. Sedangkan buku berjudul “Ayo Berolahraga” diserahkan setelah Program ini berakhir hanya akan mendapatkan total <i>fee</i> Rp3.000.000 (3000 baca x Rp1000)
+                                    </p>
+                                                                        
+                                </div>   
+                                <div class="px-4 my-3 ml-4">                                                                                                    
+                                    <p class="d-none d-sm-block">
+                                        <b>b.</b> Mendapatkan “Creator Hero Badge” pada profil dan Buku kreator.
+                                    </p> 
+                                </div>                             
+                                <div class="px-4 my-3 ml-4">                                                                                                    
+                                    <p class="d-none d-sm-block">
+                                        <b>c.</b> Mendapat <i>Rewards</i> atau hak istimewa lainnya yang akan ditentukan setelah program ini berakhir.
+                                    </p> 
+                                </div>                             
+                                
+                            </div>                                                          
+                            {{-- End of Part 5 --}}  
+
+                            <h3>6. Periode Program </h3>
+
+                            <div class="px-4 my-3 ml-4">                                
+                                <p>
+                                    <b>a.</b> Program dimulai pada tanggal 14 Februari 2021.
+                                </p>                                
+                            </div>                            
+                            <div class="px-4 my-3 ml-4">                                
+                                <p>
+                                    <b>b.</b> Program akan berakhir dalam waktu yang belum ditentukan dan/atau PIKO telah melakukan aktifitas monetisasi.
+                                </p>                                
+                            </div>                            
+                            <div class="px-4 my-3 ml-4">                                
+                                <p>
+                                    <b>c.</b> Kreator dapat secara sepihak keluar dari program ini dengan mengajukan surat pengunduran diri kepada PIKO.
+                                </p>                                
+                            </div>            
+                            <div class="px-4 my-3 ml-4">                                
+                                <p>
+                                    <b>d.</b> PIKO dapat secara sepihak mengeluarkan kreator dalam program ini jika ditemukan hal-hal yang melanggar syarat dan ketentuan pada bagian ke-2.
+                                </p>                                
+                            </div>                                  
+                            {{-- End of Part 6 --}}  
+
+                            <h3>7. Perubahan Syarat dan Ketentuan</h3>
+                            <div class="my-3 ml-4">   
+                                <p>
+                                    PIKO dapat setiap saat mengubah syarat dan ketentuan ini dengan memberikan pemberitahuan dan menerbitkan versi terbaru di situs Web dan/atau Aplikasi PIKO.
+                                </p>                                
+                            </div>                                                          
+                            {{-- End of Part 7 --}}  
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                        </div>
+                    </div>                                       
+                </article>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<!-- ***** Blog Area End ***** -->
+@endsection
+
+@section('scripts')
+<!-- ***** All jQuery Plugins ***** -->
+
+<!-- jQuery(necessary for all JavaScript plugins) -->
+<script src="{{ asset('assets/js/jquery/jquery-3.3.1.min.js')}}"></script>
+
+<!-- Bootstrap js -->
+<script src="{{ asset('assets/js/bootstrap/popper.min.js')}}"></script>
+<script src="{{ asset('assets/js/bootstrap/bootstrap.min.js')}}"></script>
+
+<!-- Plugins js -->
+<script src="{{ asset('assets/js/plugins/plugins.min.js')}}"></script>
+
+<!-- Active js -->
+<script src="{{ asset('assets/js/active.js')}}"></script>
+
+@endsection
